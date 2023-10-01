38f37e20a4cdada8b33cef3ad8352adc56eda28e
manh20051151 <nguyenvietmanh1409@gmail.com>
2023-10-02 02:57:02 +0700

    init

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Button, StyleSheet, Text, View } from 'react-native'; 
+
+import FirstScreen from './screen/FirstScreen'
+import a_1 from './screen/1_a'
+
+const  Stack = createStackNavigator();
+
+const Home = ({navigation}) => {
+  return(
+    <View>
+        <Button title='FirstScreen' 
+          onPress={()=>{
+            navigation.navigate('FirstScreen');
+          }}
+        />
+        <Button title='a_1' 
+          onPress={()=>{
+            navigation.navigate('a_1');
+          }}
+        />
+    </View>
+  )
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home}></Stack.Screen>
+        
+        <Stack.Screen name="FirstScreen" component={FirstScreen}></Stack.Screen>
+        <Stack.Screen name="a_1" component={a_1}></Stack.Screen>
+
+      </Stack.Navigator>
+
+    </NavigationContainer>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
